154a48422626203f70e7e17233e9537ed1c5d0e1
Twelfthonehone <95571424+honehone12@users.noreply.github.com>
2025-11-03 12:43:34 +0900

    added spring

---
+++ pasuki/src/test/java/mini/pasuki/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.pasuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
