84aa226f7dad9bfc617c6a61671fde1c1437a522
Twelfthonehone <95571424+honehone12@users.noreply.github.com>
2025-11-03 14:16:48 +0900

    added instants

---
+++ pasuki/src/main/java/mini/pasuki/Instants.java
@@ -0,0 +1,21 @@
+package mini.pasuki;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EntityListeners;
+
+@EntityListeners(AuditingEntityListener.class)
+@Embeddable
+public record Instants(
+        @CreatedDate
+        Instant createdAt,
+        @LastModifiedDate
+        Instant updatedAt,
+        Instant deletedAt) {
+
+}
