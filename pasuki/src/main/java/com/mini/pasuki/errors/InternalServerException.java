3334ab829a9dbea744e7ecd0cf65032b557bc838
Twelfthonehone <95571424+honehone12@users.noreply.github.com>
2025-11-03 15:59:57 +0900

    added errors

---
+++ pasuki/src/main/java/com/mini/pasuki/errors/InternalServerException.java
@@ -0,0 +1,13 @@
+package com.mini.pasuki.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerException extends Exception {
+
+    @Override
+    public String toString() {
+        return "internal server error";
+    }
+}
